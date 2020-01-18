b5cf3283cd07b3a05997217a8d5c78221a9aae36
alexthescott <43523995+alexthescott@users.noreply.github.com>
2020-01-18 12:32:18 -0800

    Added existing project

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# Sprintify_Website
-Flask Spotify Playlist Filtering
 This code was based on these repositories:
     https://github.com/datademofun/spotify-flask
     https://github.com/drshrey/spotify-flask-auth-example
---
+++ app.py
@@ -0,0 +1,97 @@
+'''
+    This code was based on these repositories:
+        https://github.com/datademofun/spotify-flask
+        https://github.com/drshrey/spotify-flask-auth-example
+        https://github.com/mari-linhares/spotify-flask
+'''
+
+from flask import Flask, request, redirect, g, render_template, session
+from spotify_requests import spotify
+import spotipy
+import spotipy.util as util
+
+
+app = Flask(__name__)
+app.secret_key = 'some key for session'
+
+# ----------------------- AUTH API PROCEDURE -------------------------
+
+@app.route("/auth")
+def auth():
+    return redirect(spotify.AUTH_URL)
+
+
+@app.route("/callback/")
+def callback():
+
+    auth_token = request.args['code']
+    auth_header = spotify.authorize(auth_token)
+    session['auth_header'] = auth_header
+
+    return profile()
+
+def valid_token(resp):
+    return resp is not None and not 'error' in resp
+
+# -------------------------- API REQUESTS ----------------------------
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+@app.route('/search/')
+def search():
+    try:
+        search_type = request.args['search_type']
+        name = request.args['name']
+        return make_search(search_type, name)
+    except:
+        return render_template('search.html')
+
+
+@app.route('/search/<search_type>/<name>')
+def search_item(search_type, name):
+    return make_search(search_type, name)
+
+
+def make_search(search_type, name):
+    if search_type not in ['artist', 'album', 'playlist', 'track']:
+        return render_template('index.html')
+
+    data = spotify.search(search_type, name)
+    api_url = data[search_type + 's']['href']
+    items = data[search_type + 's']['items']
+
+    return render_template('search.html',
+                           name=name,
+                           results=items,
+                           api_url=api_url,
+                           search_type=search_type)
+
+
+@app.route('/profile')
+def profile():
+    if 'auth_header' in session:
+        auth_header = session['auth_header']
+        # get profile data
+        profile_data = spotify.get_users_profile(auth_header)
+
+        # get user playlist data
+        playlist_data = spotify.get_users_playlists(auth_header)
+
+        # get user recently played tracks
+        recently_played = spotify.get_users_recently_played(auth_header)
+        
+        if valid_token(recently_played):
+            return render_template("profile.html",
+                               user=profile_data,
+                               playlists=playlist_data["items"],
+                               recently_played=recently_played["items"])
+
+    return render_template('profile.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=spotify.PORT)
